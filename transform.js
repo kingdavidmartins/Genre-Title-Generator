7070e2f1909572af84adccf0547e8b39fbce829d
King <kingdavidmartins@users.noreply.github.com>
2017-08-16 11:11:52 -0400

    add transform to process largeset, added largeset to repo

--- transform.js
+++ transform.js
@@ -4,6 +4,7 @@ const fs = require('fs');
 const objify = (obj) => JSON.parse(obj);
 const structNewObj = (value) => JSON.parse(JSON.stringify(value));
 
+// for small data set
 fs.readFile('movieGenreRaw.js', 'utf8' , (err, data) => {
   let newData = _.chain(objify(data))
     .map((value, index) =>
@@ -25,4 +26,29 @@ fs.readFile('movieGenreRaw.js', 'utf8' , (err, data) => {
   // saves an array of all the movie title's from movieGenreRaw to movieTitleList.txt
   fs.writeFile('movieTitleList.txt', JSON.stringify(movieList), (err) =>
     console.log(err ? `Err: ${err}` : `list of movie title's was saved`));
+});
+
+
+// for large data set
+fs.readFile('movieGenreRawLarge.js', 'utf8' , (err, data) => {
+  let newData = _.chain(objify(data))
+    .map((value, index) =>
+      structNewObj({
+        title: value.title.slice(0, -7),
+        genres: (value.genres.indexOf('|') === -1 ? value.genres : value.genres.split('|'))
+      }))
+    .value();
+
+  let movieList = _.chain(objify(data))
+    .map((value, index) => value.title.slice(0, -7))
+    .value();
+
+
+  // saves an array of objects from movieGenreRawLarge to movieGenreTransformLarge.txt in my formatting
+  fs.writeFile('movieGenreTransformLarge.txt', JSON.stringify(newData), (err) =>
+    console.log(err ? `Err: ${err}` : `transform of large file was saved`));
+
+  // saves an array of all the movie title's from movieGenreRawLarge to movieTitleListLarge.txt
+  fs.writeFile('movieTitleListLarge.txt', JSON.stringify(movieList), (err) =>
+    console.log(err ? `Err: ${err}` : `list of large movie title's was saved`));
 });
